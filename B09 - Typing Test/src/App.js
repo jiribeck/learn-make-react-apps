288c6c6f881bacf7ca0043bcf3981b9a0c14c699
Jiri Beck <jb@jiribeck.com>
2021-07-16 13:46:39 +0200

    B09 - DONE!

--- B09 - Typing Test/src/App.js
+++ B09 - Typing Test/src/App.js
@@ -1,30 +1,101 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import useCountDown from 'react-countdown-hook';
+
 import './App.css';
 
 // to calculate typing speed
 // words typed / minutes
 // words typed = (characters - typos) / 5
 
-const secondsToCount = 10;
-const paragraph = `Coding is the best. We are able to build something from scratch. It is literally imagination incarnate. Solving our own problems through coding is one of the coolest things we could do!`;
+const secondsToCount = 2;
+
+const paragraph = `Hello world! Coding is the best. We are able to build something from scratch. It is literally imagination incarnate. Solving our own problems through coding is one of the coolest things we could do!`;
 
 export default function App() {
+  const [timeLeft, { start, reset }] = useCountDown(secondsToCount * 1000, 100);
+
+  const [typedText, setTypedText] = useState('');
+  const [typoIndexes, setTypoIndexes] = useState([]);
+
+  const [wpm, setWpm] = useState();
+
+  function findTypos(str1, str2) {
+    let typos = [];
+    str2.split('').forEach((character, index) => {
+      if (character !== str1.split('')[index]) typos.push(index);
+    });
+    return typos;
+  }
+
+  function startTimer() {
+    setTypedText('');
+    start();
+  }
+
+  function resetTimer() {
+    setTypedText('');
+    setWpm();
+    reset();
+  }
+
+  // Finding Typos
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    setTypoIndexes(findTypos(paragraph, typedText));
+  }, [typedText]);
+
+  // Showing alert and counting WPM
+  useEffect(() => {
+    if (timeLeft !== 0 || typedText.length === 0) return;
+
+    // Calculate WPM
+    const wordsTyped = (typedText.length - typoIndexes.length) / 5;
+    const minMultiplier = 60 / secondsToCount;
+    setWpm((wordsTyped * minMultiplier).toFixed(2));
+  }, [wpm, timeLeft, typedText.length, typoIndexes.length]);
+
   return (
     <div className="app">
       {/* sidebar */}
       <div className="sidebar">
-        <div className="timer">00</div>
-        <button className="start">Start</button>
-        <button className="reset">Reset</button>
+        <div className="timer">{(timeLeft / 1000).toFixed(2)}</div>
+        <button className="start" onClick={() => startTimer()}>
+          Start
+        </button>
+        <button className="reset" onClick={() => resetTimer()}>
+          Reset
+        </button>
+        {wpm && <div>You typed at: {wpm}</div>}
       </div>
 
       <div className="content">
         {/* show the paragraph */}
-        <p>{paragraph}</p>
+        <p>
+          {paragraph.split('').map((character, index) => {
+            let characterClass = '';
+            const hasBeenTyped = index < typedText.split('').length;
+            if (hasBeenTyped) {
+              const isTypo = typoIndexes.includes(index);
+              characterClass = isTypo ? 'incorrect' : 'correct';
+            }
+
+            return (
+              <span key={index} className={characterClass}>
+                {character}
+              </span>
+            );
+          })}
+        </p>
 
         {/* show the textarea */}
         <form>
-          <textarea rows="10" placeholder="Test your typing skills..." />
+          <textarea
+            value={typedText}
+            onChange={(e) => setTypedText(e.target.value)}
+            rows="10"
+            placeholder="Test your typing skills..."
+          />
         </form>
       </div>
     </div>
